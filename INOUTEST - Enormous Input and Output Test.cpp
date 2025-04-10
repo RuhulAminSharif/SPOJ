434821209d4ea656392abdae82901ac9a0636710
Sharif <sharif.cse.ras@gmail.com>
2025-04-10 16:28:28 +0600

    (+)INOUTEST - Enormous Input and Output Test

---
+++ INOUTEST - Enormous Input and Output Test.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long int
+#define ld long double
+#define inf 1e18
+#define PI acos(-1)
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n; cin >> n;
+    while( n-- ) {
+        int a, b; cin >> a >> b;
+        cout << a * b << endl;
+    }
+    return 0;
+}
+// https://www.spoj.com/problems/INOUTEST/
